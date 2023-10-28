8e09fa7eb54de0199dcc58fe0e22a1a8dd1bd725
Guilherme Félix <felixguilherme452@gmail.com>
2023-10-27 21:28:29 -0300

    muro corrigido

--- prefabs/itens/construcoes/scripts/Muro.gd
+++ prefabs/itens/construcoes/scripts/Muro.gd
@@ -7,19 +7,40 @@ export var ConstruirMuro:bool = true
 
 
 func _ready():
+	self.position.y = 156
 	if ConstruirMuro:
 		yield(get_tree().create_timer(2), "timeout")
+		var menorDistancia = null
+		var muro = null
 		for node in get_parent().get_children():
 			if node.is_in_group("Muro") and node.name != self.name:
-				var distancia = abs(self.position.x - node.position.x)/16
-				var direcao = (node.position - self.position).normalized().x
-				for i in range(distancia +1):
-					var novoMuroMeio = MuroMeio.instance()
-					novoMuroMeio.position = self.position + (Vector2(16, 0) * i * direcao)
-					novoMuroMeio.set_z_index(-2)
-					get_parent().add_child(novoMuroMeio)
-					yield(get_tree().create_timer(0.3), "timeout")
-				break
+				var distancia = abs(self.position.x - node.position.x)
+				print(distancia)
+				if menorDistancia == null:
+					if self.position.x > 0 and self.position.x > node.position.x:
+						menorDistancia = distancia
+						muro = node
+					elif self.position.x < 0 and self.position.x < node.position.x:
+						menorDistancia = distancia
+						muro = node
+					else:
+						continue
+				if menorDistancia > distancia:
+					if self.position.x > 0 and self.position.x > node.position.x:
+						menorDistancia = distancia
+						muro = node
+					elif self.position.x < 0 and self.position.x < node.position.x:
+						menorDistancia = distancia
+						muro = node
+		
+		var distancia = abs(self.position.x - muro.position.x)/16
+		var direcao = (muro.position - self.position).normalized().x
+		for i in range(distancia +1):
+			var novoMuroMeio = MuroMeio.instance()
+			novoMuroMeio.position = self.position + (Vector2(16, 0) * i * direcao)
+			novoMuroMeio.set_z_index(-2)
+			get_parent().add_child(novoMuroMeio)
+			yield(get_tree().create_timer(0.3), "timeout")
 
 
 #func _process(delta):
